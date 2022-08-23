01635c379ad0eeea8477ed41f960b6b464047851
Sujitha-T <216t.sujitha@gmail.com>
2022-08-23 13:24:00 +0530

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,7 +9,10 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'my-app';
 
-
+  images=['https://media.istockphoto.com/photos/pretty-young-woman-stylishly-dressed-in-a-straw-hat-with-color-bags-picture-id1033729610?k=20&m=1033729610&s=612x612&w=0&h=56u-S8LPU2gL5CO3kSF9wq_PV4kSY5oVl7Hhm-0fshg=',
+  'https://wallpaperaccess.com/full/2483945.jpg',
+  'https://media.istockphoto.com/photos/hand-touching-to-virtual-info-graphics-with-trolley-cart-icons-picture-id1312231371?b=1&k=20&m=1312231371&s=170667a&w=0&h=kgZdxKibjXB2QJaGeX9pk_DyzbHr6smurVY9oQ9gFHo='];
+  
 
 
 }
